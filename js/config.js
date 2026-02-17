a56e121d43363bd971d4d3f4a5a55eb35470c466
ColeBoelterGIS <cboelter@stillwatermn.gov>
2026-02-17 16:01:08 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 
-export const APP_VERSION = '2026-02-17.3';
+export const APP_VERSION = '2026-02-17.4';
 
 
 
@@ -84,6 +84,7 @@ export const BOUNDARY = Object.freeze({
     fillOpacity: 0
   })
 });
+
 
 
 
